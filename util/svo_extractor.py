3f9afcc40fdb3c3dd8c49a177e6ff9cc2b333df7
Demon702 <debanjanmondal702@gmail.com>
2023-03-30 19:55:57 -0400

    Add pep3k normal svo data

--- util/svo_extractor.py
+++ util/svo_extractor.py
@@ -25,7 +25,7 @@ if not os.path.exists(out_dir):
 
 def get_subject_phrase(doc):
     for token in doc:
-        if ('subj' in token.dep_):
+        if (token.dep_ in ['nsubj', 'compound']):
             # subtree = list(token.subtree)
             # start = subtree[0].i
             # end = subtree[-1].i + 1
@@ -34,7 +34,7 @@ def get_subject_phrase(doc):
 
 def get_object_phrase(doc):
     for token in doc:
-        if ('obj' in token.dep_):
+        if (token.dep_ in ['dobj', 'pobj','iobj', 'attr', 'oprd', 'dative', 'xcomp'] or (token.pos_ in ['NOUN', 'PROPN'] and token.dep_ == 'ROOT')):
             # subtree = list(token.subtree)
             # start = subtree[0].i
             # end = subtree[-1].i + 1
@@ -56,15 +56,15 @@ def get_neg(doc):
 docs = list(nlp.pipe(sentences))
 
 
-for i, doc in enumerate(docs):
-    sent = sentences[i]
-    print("Sentence: {}".format(sentences[i]))
+for i, sent in enumerate(sentences):
+    doc = nlp(sent)
+    print("Sentence: {}".format(sent))
     sub = get_subject_phrase(doc)
     obj = get_object_phrase(doc)
     verb = get_verb(doc)
     neg = get_neg(doc)
-
-    if not sub or not obj or not verb:
+    print(sub, obj, verb)
+    if not sub and not obj and not verb:
         arr = sent.split()
         if len(arr) == 3:
             sub = arr[0]
