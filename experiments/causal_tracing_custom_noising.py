9540494a77542fbaf685be58d60b90f7620e5693
Demon702 <debanjanmondal702@gmail.com>
2023-04-13 15:43:29 -0400

    Change make inputs in causal tracing to left pad inputs

--- experiments/causal_tracing_custom_noising.py
+++ experiments/causal_tracing_custom_noising.py
@@ -634,20 +634,16 @@ def plot_all_flow(mt, prompt, subject=None):
 
 # Utilities for dealing with tokens
 def make_inputs(tokenizer, prompts, device="cuda"):
-    # token_lists = [tokenizer.encode(p) for p in prompts]
-    # maxlen = max(len(t) for t in token_lists)
-    # input_ids = [[tokenizer.pad_token] * (maxlen - len(t)) + t for t in token_lists]
-    # # position_ids = [[0] * (maxlen - len(t)) + list(range(len(t))) for t in token_lists]
-    # attention_mask = [[0] * (maxlen - len(t)) + [1] * len(t) for t in token_lists]
-    # return dict(
-    #     input_ids=torch.tensor(input_ids).to(device),
-    #     #    position_ids=torch.tensor(position_ids).to(device),
-    #     attention_mask=torch.tensor(attention_mask).to(device),
-    # )
-    inp = tokenizer(prompts, return_tensors='pt', max_length=16, padding="max_length", truncation=True)
-    inp = { k: v.to(device) for k, v in inp.items() }
-    return inp
-
+    token_lists = [tokenizer.encode(p) for p in prompts]
+    maxlen = max(len(t) for t in token_lists)
+    input_ids = [[tokenizer.pad_token] * (maxlen - len(t)) + t for t in token_lists]
+    # position_ids = [[0] * (maxlen - len(t)) + list(range(len(t))) for t in token_lists]
+    attention_mask = [[0] * (maxlen - len(t)) + [1] * len(t) for t in token_lists]
+    return dict(
+        input_ids=torch.tensor(input_ids).to(device),
+        #    position_ids=torch.tensor(position_ids).to(device),
+        attention_mask=torch.tensor(attention_mask).to(device),
+    )
 
 def decode_tokens(tokenizer, token_array):
     if hasattr(token_array, "shape") and len(token_array.shape) > 1:
