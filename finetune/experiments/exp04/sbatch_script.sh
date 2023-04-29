3338bccd5eb8a4af9e78c01f0239677b714c02fe
Demon702 <debanjanmondal702@gmail.com>
2023-04-29 00:57:19 -0400

    Edit the sbatch script for exp04

--- finetune/experiments/exp04/sbatch_script.sh
+++ finetune/experiments/exp04/sbatch_script.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #
-#SBATCH --job-name=finetune_gpt
+#SBATCH --job-name=finetune_incorrect_dev_gpt
 #SBATCH --partition=gypsum-m40
 #SBATCH --gres=gpu:1
 #SBATCH --mem=50G
 #SBATCH --cpus-per-task=5
-#SBATCH -o result/logs/fine_tune_large_inversed-slurm-%j.out
+#SBATCH -o result/logs/finetune_incorrect_dev_gpt_large_normal-%j.out
 module load miniconda
 conda activate memit
 
-# python3 fine_tune_gpt.py --dataset=20q --datatype=normal --log2_batch_size=6 --model=gpt2-large --evaluation_only --run_name=captain-resistance-1374
-python fine_tune_gpt.py --dataset=pep3k --datatype=inversed --epochs=10 --learning_rate=2.3917596836828713e-05 --log2_batch_size=5 --model=gpt2-large --save_model
+python fine_tune_gpt.py --dataset=pep3k --datatype=inversed --epochs=10 --learning_rate=2.3917596836828713e-05 --log2_batch_size=5 --model=gpt2-large --save_model \
+--checkpoint /work/pi_adrozdov_umass_edu/debanjanmond_umass_edu/memit_commonsense/finetune/experiments/exp03/result/best-checkpoints/pep3k/gpt2-large/normal/
