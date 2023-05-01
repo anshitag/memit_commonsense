09241a6477171b21fb710e65c47abbb8c029c0c3
Anshita Gupta <guptaanshita8@gmail.com>
2023-04-30 21:15:09 -0400

    Set seed in editing code

--- experiments/evaluate_cs.py
+++ experiments/evaluate_cs.py
@@ -1,4 +1,4 @@
-import json
+import json, os
 import shutil
 from itertools import islice
 from time import time
@@ -257,6 +257,17 @@ def chunks(arr, n):
     for i in range(0, len(arr), n):
         yield arr[i : i + n]
 
+def set_seed(seed: int = 42) -> None:
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    # When running on the CuDNN backend, two further options must be set
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    # Set a fixed value for the hash seed
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    print(f"Random seed set as {seed}")
+
+
 
 if __name__ == "__main__":
     import argparse
@@ -414,6 +425,7 @@ if __name__ == "__main__":
 
     parser.set_defaults(skip_generation_tests=False, conserve_memory=False)
     args = parser.parse_args()
+    set_seed()
 
     if args.wandb_active:
         wandb.init(project=args.wandb_project, entity=args.wandb_account, config=args)
