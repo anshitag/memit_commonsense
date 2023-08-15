dc8ff8fe25a26760e4a9591432f444e9eb45ad36
Akshay-Krishna-S <asheshadri@umass.edu>
2023-08-15 04:36:19 -0400

    Adding executable script file for repair finetuning

--- repair_finetune_experiments/sbatch_script.sh
+++ repair_finetune_experiments/sbatch_script.sh
@@ -1,13 +1,31 @@
 #!/bin/bash
-#
-#SBATCH --job-name=finetune_incorrect_dev_gpt
-#SBATCH --partition=gypsum-m40
-#SBATCH --gres=gpu:1
-#SBATCH --mem=50G
-#SBATCH --cpus-per-task=5
-#SBATCH -o result/logs/finetune_incorrect_dev_gpt_large_normal-%j.out
-module load miniconda
-conda activate memit
 
-python fine_tune_gpt.py --dataset=pep3k --datatype=inversed --epochs=10 --learning_rate=2.3917596836828713e-05 --log2_batch_size=5 --model=gpt2-large --save_model \
---checkpoint /work/pi_adrozdov_umass_edu/debanjanmond_umass_edu/memit_commonsense/finetune/experiments/exp03/result/best-checkpoints/pep3k/gpt2-large/normal/
+
+#Example script for running repair finetuning on the base finetuned gpt2-xl model for the 20q datasets
+python fine_tune_gpt.py \
+--model gpt2-xl \
+--dataset 20q \
+--log2_batch_size 5 \
+--epochs 9 \
+--learning_rate 1.5900193740411122e-06 \
+--checkpoint ../base_finetune_experiments/result/checkpoint/ \
+--save_path ./result/checkpoint/ \
+--output_path ./result/output/ \
+--train_file ../data/20q/incorrect_subsets/gpt2-xl/edit_validation_subset.json \
+--valid_file ../data/20q/edit_validation_set.json \
+--save_model
+
+
+#Example script for evaluating repair finetuning on repaired gpt2-xl model for the affected metrics for the 20q datasets
+python evaluate_affected_finetune_model.py \
+--model gpt2-xl \
+--model_path ./result/checkpoint/ \
+--input_file ../data/20q/probe_set.json
+
+
+#Example script for evaluating repair finetuning on repaired gpt2-xl model for the unaffected metrics for the 20q datasets
+python evaluate_unaffected_finetune_model.py \
+--model gpt2-xl \
+--edit_model_path ./result/checkpoint/ \
+--model_path ../base_finetune_experiments/result/checkpoint/ \
+--input_file ../data/20q/probe_set.json
